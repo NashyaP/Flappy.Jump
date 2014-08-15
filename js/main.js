41e345adca9307fa195d3378d44436c6e45f5b61
nashya2@live.com <>
2014-08-15 15:41:22 +0000

    Flappy.Jump File Completed

--- js/main.js
+++ js/main.js
@@ -39,9 +39,23 @@ var mainState = {
     
     this.bird.body.gravity.y = 1000;
     
-    var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     
+    this.pipes = game.add.group();
+    
+    this.pipes.enableBody = true;
+    
+    //Create 20 pipes to hold in the group
+    this.pipes.createMultiple(20, 'pipe');
+    
+    //Add in pipes over 1.5 seconds to the screen
+    this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+    
+    //When spacebar in pressed, make the bird jump!
+    var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
     spaceKey.onDown.add(this.jump, this);
+    
+    this.score = 0;
+    this.labelScore = game.add.text(20,20, "0", {font: "30px Georgia", fill:"#ffffff"}); 
 },
     
   update: function () {
@@ -52,10 +66,34 @@ var mainState = {
   if (this.bird.inWorld == false) {
       this.restartGame();  
     }
-
+game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this);
 
 },
+addOnePipe: function (x,y) {
+    //Get the first dead pipe in our group
+    var pipe = this.pipes.getFirstDead();
+    
+    //Set the new position
+    pipe.reset(x,y);
+    
+    pipe.body.velocity.x = -200;
+    
+    pipe.checkWorldBounds = true;
+    pipe.outOfBoundsKill = true;
+},
 
+addRowOfPipes: function () {
+  var hole = Math.floor(Math.random() * 5) + 1;
+  
+   for(var i = 0; i < 8; i++)
+   if (i != hole && i != hole +1) {
+     
+     this.addOnePipe(400, i*60+10);
+   }
+   
+   this.score +=1; 
+   this.labelScore.text = this.score;
+},
 
 jump: function () {
   
